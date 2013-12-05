59253632d67588584628c3e0fe1ea1180d931104
Carlos Interone <carlos@mostlyclicks.com>
2013-12-05 08:41:45 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Virq10::Application.config.secret_token = '6b7ec97b03033dbf8efa499cbf15af6aea8a5f313782fc27d18cde9f6d86483f1708d1fe2780bbd915bb9cc6b0373195f0273176d7c78066b5d49dcc4d86d644'
