d7fd77da5f672717d5a4b200a1057f58d610c53d
Carlos Interone <carlos@mostlyclicks.com>
2013-12-09 09:18:49 -0600

    add footer menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,5 +11,15 @@ module ApplicationHelper
     presenter.max_depth = 0 # prevents dropdown menus, which don't render correctly
     presenter
   end
+
+  def footer_menu
+    menu_items = Refinery::Menu.new(Refinery::Page.footer_menu_pages)
+
+    presenter = Refinery::Pages::MenuPresenter.new(menu_items, self)
+    presenter.dom_id = "footer_menu"
+    presenter.css = "footer_menu"
+    presenter.menu_tag = :div
+    presenter
+  end
   
 end
